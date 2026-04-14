52611336ba9542658386e55869a94cd400f24c68
Zain Raza <100517579+Zain-128@users.noreply.github.com>
2026-04-14 13:57:41 -0400

    feat:implemented auth handler.

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -13,10 +13,10 @@ router.post("/verify-otp", verifyOtpController);
 router.post("/resend-otp", resendOtpController);
 router.post("/reset-password", resetPasswordController);
 
-router.get("/get-user-with-search", getUserWithSearchController);
 router.use(AuthHandler);
 router.delete("/delete-user", deleteUserController);
 router.get("/get-auth-user", getUserController);
 router.put("/change-password",  changePasswordController);
 router.put("/update-user", upload.single("profilePicture"), updateUserController);
+router.get("/get-user-with-search", getUserWithSearchController);
 export default router;
